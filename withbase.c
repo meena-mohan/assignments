0690203ab151cfd4435969be9fdc4d8ae44c56d2
meena-mohan <m.meenamohan1995@gmail.com>
2016-01-26 18:19:48 +0530

    Create withbase.c

---
+++ withbase.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main(void)
+{
+	char a[100];
+    int i=0,n,j,m,rem;
+    printf("Number:");
+    scanf("%d\n",&n);
+    printf("Base:");
+    scanf("%d\n",&m);
+    while(n!=0)
+    {
+    	rem=n%m;
+    	if(rem<10)
+    	{
+    		a[i]=rem+'0';
+    	}
+    	else
+    	{
+    		a[i]=rem+55;
+    	}
+    	i++;
+    	n=n/m;
+    }
+    for(j=i-1;j>=0;j--)
+    {
+    	printf("%c",a[j]);
+    }
+	return 0;
+}
