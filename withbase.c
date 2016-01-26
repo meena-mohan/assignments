f3dd3efc39ba394063c26ce58915cc723e7d123b
meena-mohan <m.meenamohan1995@gmail.com>
2016-01-26 18:26:18 +0530

    Update withbase.c

--- withbase.c
+++ withbase.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-	char a[100];
+    char a[100];
     int i=0,n,j,m,rem;
     printf("Number:");
     scanf("%d\n",&n);
@@ -12,13 +12,12 @@ int main(void)
     	rem=n%m;
     	if(rem<10)
     	{
-    		a[i]=rem+'0';
+    		a[i++]=rem+'0';
     	}
     	else
     	{
-    		a[i]=rem+55;
+    		a[i++]=rem+55;
     	}
-    	i++;
     	n=n/m;
     }
     for(j=i-1;j>=0;j--)
